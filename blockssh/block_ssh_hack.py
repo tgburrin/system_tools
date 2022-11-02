6648cdb6eb6c03d44dabf3bc31376e346ffaf0a1
Tim Burrington <tgburrin>
2022-11-01 22:28:52 -0400

    Reverting changes to use an external module.  Going back to the more reliable cli for nft.

--- blockssh/block_ssh_hack.py
+++ blockssh/block_ssh_hack.py
@@ -10,7 +10,6 @@ import re
 import sqlite3
 import json
 import logging.handlers
-from socket import AF_INET
 
 import yaml
 
@@ -36,16 +35,30 @@ def run_nft_command(cmd):
     if cmdres.returncode != 0:
         raise ValueError(f"Error: {cmdres.stderr}")
 
-    payload = json.loads(cmdres.stdout)['nftables']
-    metadata = payload.pop(0)
+    payload = {}
+    metadata = {}
+    if cmdres.stdout != b'':
+        payload = json.loads(cmdres.stdout)['nftables']
+        metadata = payload.pop(0)
+
     return payload, metadata
 
 
-def get_current_block_list():
-    nftable = PROCESS_CFG['nft_table']
-    block_set = PROCESS_CFG['nft_auth_log_set']
+def get_block_set_details():
+    nftable = PROCESS_CFG['nf_table']
+    block_set = PROCESS_CFG['nf_auth_log_set']
+    payload, metadata = run_nft_command(f"list set ip {nftable} {block_set}")
+    rv = payload[0].get('set')
+    if 'elem' in rv:
+        del rv['elem']
+    return rv
 
-    payload, metadata = run_nft_command(f"list set inet {nftable} {block_set}")
+
+def get_current_block_list():
+    nftable = PROCESS_CFG['nf_table']
+    block_set = PROCESS_CFG['nf_auth_log_set']
+
+    payload, metadata = run_nft_command(f"list set ip {nftable} {block_set}")
     rv = []
     for ipelem in payload[0].get('set').get('elem'):
         rv.append((ipelem.get('elem', {}).get('val')))
@@ -53,18 +66,29 @@ def get_current_block_list():
 
 
 def list_table_sets():
-    payload, metadata = run_nft_command("list sets inet")
+    payload, metadata = run_nft_command("list sets ip")
     rv = []
     for setinfo in payload:
         setdetails = setinfo.get('set')
-        if setdetails.get('table') == PROCESS_CFG['nft_table']:
+        if setdetails.get('table') == PROCESS_CFG['nf_table']:
             rv.append(setdetails.get('name'))
 
     return rv
 
 
 def init_block_set():
+    nf_set = PROCESS_CFG['nf_auth_log_set']
+    nf_table = PROCESS_CFG['nf_table']
     table_sets = list_table_sets()
+    if nf_set not in table_sets:
+        payload, metadata = run_nft_command(
+                f"add set ip {nf_table} {nf_set} {{ type ipv4_addr; flags dynamic, timeout;}}"
+            )
+    else:
+        set_details = get_block_set_details()
+        assert set_details.get('family') == 'ip', 'address family is not ipv4'
+        assert set_details.get('type') == 'ipv4_addr', 'the set must be created to contain ipv4 addresses'
+        assert 'timeout' in set_details.get('flags'), 'the set must be configured to allow timeouts'
 
 
 def build_regex_list(pattern_list):
@@ -75,12 +99,12 @@ def build_regex_list(pattern_list):
 def check_log_line(log_line, pattern_list):
     rv = False
     details = {"hostname": None, "username": None, "ip": None}
-
-    log_line = log_line[16:].rstrip()  # 15 char timestamp + 1 space
     for p in pattern_list:
         m = re.match(p, log_line)
         if m:
+            log.debug(f"Log line {log_line} matches")
             match_parts = m.groupdict()
+            log.debug(f"{match_parts}")
             if match_parts.get('HOSTNAME'):
                 details['hostname'] = match_parts.get('HOSTNAME')
             if match_parts.get('USERNAME'):
@@ -99,17 +123,13 @@ def stop_running(signum, frame):
 
 
 def block_ip(addr):
-    nftable = PROCESS_CFG['nft_table']
-    block_set = PROCESS_CFG['nft_auth_log_set']
+    log.info(f"Adding {addr} to list of application blocks")
+    nf_set = PROCESS_CFG['nf_auth_log_set']
+    nf_table = PROCESS_CFG['nf_table']
 
-    log.info(f"blocking {addr}")
-    with nftables.main.NFTables(nfgen_family=AF_INET) as nft:
-        nft.set_elems(
-            "add",
-            table=nftable,
-            set=block_set,
-            elements={addr},
-        )
+    expr = f"{addr} timeout {PROCESS_CFG['nf_duration_min']}m"
+    cmd = f"add element ip {nf_table} {nf_set} {{ {expr} }}"
+    payload, metadata = run_nft_command(cmd)
     return True
 
 
@@ -122,29 +142,52 @@ def process_log_lines(ifd, patterns):
         if line.startswith(MYHOST):  # this eliminates multiline syslog statements
             ismatch, addr_details = check_log_line(line, patterns)
             if ismatch:
+                log.debug(f"Adding {addr_details} from {line}")
                 rv.append(addr_details)
+        raw_line = ifd.readline()
     return rv
 
 
 def process_messages(msgs):
     c = STATS_DB.cursor()
+    updatesql = """update ssh_drops_counter set
+        block_count = block_count+1,
+        last_block_time = CURRENT_TIMESTAMP
+    where block_address = ?"""
+
+    insertsql = """insert or ignore into ssh_drops_counter (block_address) values (?)"""
 
     blockips = set()
     for msg in msgs:
-        if msg.get('ip') in blockips:
+        ip = msg.get('ip')
+        if ip in blockips:
             continue
 
-        c.execute("update ssh_drops_counter set block_count = block_count+1 where block_address = ?", (msg.get('ip'), ))
+        c.execute(updatesql, (ip, ))
         if not c.rowcount:
-            c.execute("insert or ignore into ssh_drops_counter values (?,1)", (msg.get('ip'), ))
-
+            c.execute(insertsql, (ip, ))
         blockips.add(msg.get('ip'))
+        block_ip(ip)
 
     c.close()
     STATS_DB.commit()
-    for ip in blockips:
-        block_ip(ip)
 
+
+def create_stats_table():
+    c = STATS_DB.cursor()
+
+    table_def = """create table ssh_drops_counter (
+    block_address text primary key,
+    block_count integer not null default 1,
+    last_block_time datetime not null default CURRENT_TIMESTAMP )"""
+
+    res = c.execute("SELECT name FROM sqlite_master WHERE type='table'")
+    table_list = [row.get('name') for row in res]
+    if 'ssh_drops_counter' not in table_list:
+        c.execute(table_def)
+
+    c.close()
+    STATS_DB.commit()
 
 def parse_arguments() -> dict:
     parser = argparse.ArgumentParser(
@@ -165,6 +208,8 @@ def parse_arguments() -> dict:
 
 
 if __name__ == "__main__":
+    log_level = logging.INFO
+
     args = parse_arguments()
     with open(args["config"]) as fd:
         PROCESS_CFG = yaml.safe_load(fd.read())
@@ -175,18 +220,21 @@ if __name__ == "__main__":
     if not PROCESS_CFG.get('auth_log'):
         PROCESS_CFG['auth_log'] = DEFAULT_AUTH_LOG
 
-    if not PROCESS_CFG.get('nft_table') or not PROCESS_CFG.get('nft_auth_log_set'):
-        raise Exception("An nnft_table and nft_auth_log_set must be specified")
+    if not PROCESS_CFG.get('nf_table') or not PROCESS_CFG.get('nf_auth_log_set'):
+        raise Exception("An nnf_table and nf_auth_log_set must be specified")
 
-    if not PROCESS_CFG.get('nft_duration_min'):
-        PROCESS_CFG['nft_duration_min'] = DEFAULT_DURATION_MIN
+    if not PROCESS_CFG.get('nf_duration_min'):
+        PROCESS_CFG['nf_duration_min'] = DEFAULT_DURATION_MIN
+
+    if PROCESS_CFG.get('logging_level') == "DEBUG":
+        log_level = logging.DEBUG
 
     logging.basicConfig(
         stream=sys.stdout,
         format="%(asctime)s - %(levelname)s - %(message)s"
     )
     log = logging.getLogger()
-    log.setLevel(logging.INFO)
+    log.setLevel(log_level)
     # log.addHandler(logging.handlers.SysLogHandler(address='/dev/log'))
 
     log.info("Starting ssh blocking process in pid {}".format(os.getpid()))
@@ -197,6 +245,9 @@ if __name__ == "__main__":
     log.info(f"Opening state database {PROCESS_CFG.get('db_path')}")
     STATS_DB = sqlite3.connect(PROCESS_CFG.get('db_path'))
     STATS_DB.row_factory = dict_row
+
+    create_stats_table()
+    init_block_set()
 
     inode = os.stat(PROCESS_CFG['auth_log']).st_ino
     infile = open(PROCESS_CFG['auth_log'], 'r')
